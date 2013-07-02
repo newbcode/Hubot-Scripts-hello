a68b234ea5c757b0dbc8ad344e5ebb295fea4192
Yunchang Kang <codenewb@gmail.com>
2013-07-02 17:43:24 +0900

    Add hello font select..

--- lib/Hubot/Scripts/hello.pm
+++ lib/Hubot/Scripts/hello.pm
@@ -16,12 +16,15 @@ sub load {
         qr/^hello list$/i,
         \&hello_list,
     );
+    $robot->hear(
+        qr/^hello (-f) (.+)$/i,
+        \&hello_font,
+    );
+
 }
 
 sub hello {
     my $msg = shift;
-
-    my $sender = $msg->message->user->{name};
 
     my @fonts = qw/banner block big bubble digital ivrit lean mini mnemonic
         script shadow slant small smscript smshadow smslant standard term/;
@@ -33,12 +36,11 @@ sub hello {
     my $font = Text::FIGlet->new(-d=>"./figlet", -f=>"$fonts[$num]");
     my $text = $font->figify(-A=>"$user_input");
     $msg->send( split (/\n/, $text) ) if $user_input ne 'list';
+    $msg->send( split (/\n/, $text) ) if $user_input ne '-f';
 }
 
 sub hello_list {
     my $msg = shift;
-
-    my $sender = $msg->message->user->{name};
 
     my @fonts = qw/banner block big bubble digital ivrit lean mini mnemonic
         script shadow slant small smscript smshadow smslant standard term/;
@@ -47,6 +49,27 @@ sub hello_list {
     $msg->send('List of available asciifonts - '. $s_fonts );
 }
 
+sub hello_font {
+    my $msg = shift;
+
+    my $user_input = $msg->match->[1];
+
+    my $flag = 'off';
+    my @fonts = qw/banner block big bubble digital ivrit lean mini mnemonic
+        script shadow slant small smscript smshadow smslant standard term/;
+
+    my $s_fonts = join ('/ ', @fonts);
+
+    for my $font (@fonts) {
+        if ($font eq $user_input) {
+            my $font = Text::FIGlet->new(-d=>"./figlet", -f=>"$user_input");
+            my $text = $font->figify(-A=>"$user_input");
+            $msg->send( split (/\n/, $text) ) if $user_input ne 'list';
+            $flag = 'on';
+        }
+    }
+    $msg->send('List of available asciifonts - '. $s_fonts ) if $flag eq 'on';
+}
 1;
 
 =pod
@@ -59,6 +82,7 @@ sub hello_list {
 
     hello <text> - Random text show in ascii
     hello - list - Ascii Fonts List
+    hello -f <text> - Select Ascii Font to Show.  
  
 =head1 AUTHOR
 
